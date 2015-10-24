15cc4cc0d8accc55cb7a820d6422184fc2cea0e0
mdaif (mohamed.daif) <muhammad.daif@gmail.com>
2015-10-25 00:25:24 +0200

    improves style after pylint suggestions

--- chess_challenge.py
+++ chess_challenge.py
@@ -1,12 +1,20 @@
+"""Chess Challenge is a module that aims to solve a (REALLY)  generalized
+version of the famous 8-queens puzzle"""
 import datetime
 
 from helpers import king_danger, knight_danger
 
 
 class ChessChallengeEngine(object):
+    """ChessChallengeEngine is the core component of the module,
+    it is initialized by the chess pieces to be put on the chess board,
+    the board width, and the board height, running execute method results in
+    calculating the unique configurations of the pieces placement on the
+    board"""
+
     def __init__(self, pieces, board_width, board_height):
         self.pieces = sorted(
-            pieces)  # sorting lexographically to make sure all possible
+            pieces)  # sorting lexicographically to make sure all possible
         # permutations of the same problem maps to the same input
 
         self.board_width = board_width
@@ -14,12 +22,15 @@ class ChessChallengeEngine(object):
         self.all_pieces = pieces  # won't be decremented at each step
 
     def execute(self):
+        """The main engine's method, when called it runs the chess puzzle
+        algorithm, returning all unique configurations of the chess pieces
+        on the chess board"""
         solutions = [[]]
-        for row in range(self.board_height):
+        for _ in range(self.board_height):  # for each row of the board
             if self.pieces:
-                solutions, self.pieces = self.add_one_piece(self.pieces,
-                                                            self.board_height,
-                                                            solutions)
+                solutions, self.pieces = self._add_one_piece(self.pieces,
+                                                             self.board_height,
+                                                             solutions)
                 # after first iteration, we can find the inserted pieces are
                 #  less than the expected to be inserted.
                 if len(solutions) < len(self.pieces):
@@ -27,8 +38,10 @@ class ChessChallengeEngine(object):
 
         return solutions
 
-    def add_one_piece(self, pieces, columns, prev_solutions):
-        # print "previous solutions: ", prev_solutions
+    def _add_one_piece(self, pieces, columns, prev_solutions):
+        """Internal method, used by execute method to scan the board for
+        possible attacks of already placed pieces, it's run each time a new
+        piece is added."""
         current_piece = pieces[0]
         solutions = []
         seen = []
@@ -38,7 +51,7 @@ class ChessChallengeEngine(object):
             # the number of rows to be examined differs based on the type of
             #  piece being played ... we can always check for all rows but
             # it becomes very expensive when you have all queens and the
-            # number of pieces > 7 (that's an imperical observation). That
+            # number of pieces > 7 (that's an empirical observation). That
             # is a corner case and we should then only examine the row next
             # to the row we already examined.
             rows = len(solution) + 2 if set(self.all_pieces) == set([
@@ -46,8 +59,8 @@ class ChessChallengeEngine(object):
                 self.all_pieces) > 7 else self.board_height + 1
             for row in range(1, rows):
                 for column in range(1, columns + 1):
-                    if not self.under_attack(current_piece, row, column,
-                                             solution):
+                    if not self._under_attack(current_piece, row, column,
+                                              solution):
 
                         result = sorted(
                             solution + [(current_piece, row, column)])
@@ -58,77 +71,88 @@ class ChessChallengeEngine(object):
         return solutions, self.pieces
 
     @staticmethod
-    def under_attack(current_piece, row, column, candidate_solution):
+    def _under_attack(current_piece, row, column, candidate_solution):
+        """Internal method, each time _add_one_piece finds a potential
+        location (indicated by row, column) it calls this method to make
+        sure it's safe to place the current piece. Different types of
+        attacks are determined based on the current piece's type and each
+        other piece located in each sub-solution found so far"""
         for chess_piece in candidate_solution:
 
-            attacking_piece, r, c = chess_piece
+            attacking_piece, attacking_row, attacking_column = chess_piece
             if current_piece == 'Queen' or attacking_piece == 'Queen':
-                if r == row:
+                if attacking_row == row:
                     return True  # Check row
-                if c == column:
+                if attacking_column == column:
                     return True  # Check column
-                if (column - c) == (row - r):
+                if (column - attacking_column) == (row - attacking_row):
                     return True  # Check left diagonal
-                if (column - c) == -(row - r):
+                if (column - attacking_column) == -(row - attacking_row):
                     return True  # Check right diagonal
 
             elif current_piece == 'King':
-                if king_danger(r, c, row, column):
+                if king_danger(attacking_row, attacking_column, row, column):
                     return True
                 if attacking_piece == 'Rook':
-                    if r == row:
+                    if attacking_row == row:
                         return True  # Check row
-                    if c == column:
+                    if attacking_column == column:
                         return True  # Check column
-                        # print
             elif current_piece == 'Rook':
-                # print "r,c,row, col", r, c, row, column
-                if r == row:
+                if attacking_row == row:
                     return True  # Check row
-                if c == column:
+                if attacking_column == column:
                     return True  # Check column
                 if attacking_piece == 'King':
-                    if king_danger(r, c, row, column):
+                    if king_danger(attacking_row, attacking_column, row,
+                                   column):
                         return True
                 if attacking_piece == 'Knight':
-                    if knight_danger(r, c, row, column):
+                    if knight_danger(attacking_row, attacking_column, row,
+                                     column):
                         return True
 
             elif current_piece == 'Knight':
-                if knight_danger(r, c, row, column):
+                if knight_danger(attacking_row, attacking_column, row, column):
                     return True
 
                 if attacking_piece == 'Bishop':
-                    if (column - c) == (row - r):
+                    if (column - attacking_column) == (row - attacking_row):
                         return True  # Check left diagonal
-                    if (column - c) == -(row - r):
+                    if (column - attacking_column) == -(row - attacking_row):
                         return True  # Check right diagonal
 
             elif current_piece == 'Bishop' or attacking_piece == 'Bishop':
-                if (column - c) == (row - r):
+                if (column - attacking_column) == (row - attacking_row):
                     return True  # Check left diagonal
-                if (column - c) == -(row - r):
+                if (column - attacking_column) == -(row - attacking_row):
                     return True  # Check right diagonal
 
                 if attacking_piece == 'Knight':
-                    if knight_danger(r, c, row, column):
+                    if knight_danger(attacking_row, attacking_column, row,
+                                     column):
                         return True
-
         return False
 
 
-if __name__ == "__main__":
+def main():
+    """"main method, it's run if the module is executed"""
+
     start_time = datetime.datetime.now()
     #    pieces = ['Rook', 'Rook', 'Knight', 'Knight', 'Knight', 'Knight']
     # pieces = ['Knight', 'Knight', 'Bishop']
-    pieces = ['Queen'] * 8
+    input_pieces = ['Queen'] * 8
 
-    challenge = ChessChallengeEngine(pieces, 8, 8)
-    results = challenge.execute()
-    for result in results:
-        print result
+    challenge = ChessChallengeEngine(input_pieces, 8, 8)
+    end_results = challenge.execute()
+    for end_result in end_results:
+        print end_result
     end_time = datetime.datetime.now()
     print "Program finished in approximately {} " \
           "seconds, number of unique solutions " \
-          "found: {}".format((end_time - start_time).total_seconds(), 
-                             len(results))
+          "found: {}".format((end_time - start_time).total_seconds(),
+                             len(end_results))
+
+
+if __name__ == "__main__":
+    main()
--- helpers.py
+++ helpers.py
@@ -1,10 +1,19 @@
+"""A collection of helper functions to be used by the main module"""
 import math
 
 
-def king_danger(x1, y1, x2, y2):
-    return math.fabs(x1 - x2) <= 1 and math.fabs(y1 - y2) <= 1
+def king_danger(row_1, column_1, row_2, column_2):
+    """Checks the distance between a king and another piece, it returns True
+    if the distance is less than or equal one square to all directions"""
+    return math.fabs(row_1 - row_2) <= 1 and math.fabs(
+        column_1 - column_2) <= 1
 
 
-def knight_danger(x1, y1, x2, y2):
-    deltas = [(-1, -2), (1, -2), (-2, -1), (2, -1), (-2, 1), (2, 1), (-1, 2), (1, 2)]
-    return (x1 == x2 and y1 == y2) or any((x2 - x1, y2 - y1) == delta for delta in deltas)
+def knight_danger(row_1, column_1, row_2, column_2):
+    """Checks if the locations between a knight and another piece,
+    it checks for the combinations of L-shape positions and returns True if
+    one of them is found."""
+    deltas = [(-1, -2), (1, -2), (-2, -1), (2, -1), (-2, 1), (2, 1), (-1, 2),
+              (1, 2)]
+    return (row_1 == row_2 and column_1 == column_2) or any(
+        (row_2 - row_1, column_2 - column_1) == delta for delta in deltas)
